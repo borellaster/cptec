89e75979e107ddaa69dbba63e46191efbe9b36aa
borellaster <borellaster@gmail.com>
2016-12-23 14:24:11 -0200

    comentario

--- index.js
+++ index.js
@@ -16,27 +16,22 @@ app.put('/rest/countries/:id', countries.update);
 app.delete('/rest/countries/:id', countries.delete);
 
 /*states api*/
-app.get('/rest/states', states.index);
-app.get('/rest/states/:id', states.show);
-app.post('/rest/states', states.create);
+app.get('/rest/states/:page/:size', states.findAll);
+app.get('/rest/states/search/:page/:size/:name', states.search);
+app.get('/rest/states/:id', states.findById);
+app.post('/rest/states', states.save);
 app.put('/rest/states/:id', states.update);
 app.delete('/rest/states/:id', states.delete);
 
 /*cities api*/
-app.get('/rest/cities', cities.index);
-app.get('/rest/cities/:id', cities.show);
-app.post('/rest/cities', cities.create);
-app.put('/rest/cities/:id', cities.update);
-app.delete('/rest/cities/:id', cities.delete);
+
+
 
 //app.get('/rest/countries/raw/json', countries.raw);
 //usado para sql nativo...
 
 //set default port
 app.set('port', process.env.PORT || 8000);
-
-//set default size for pagination
-app.set('size', 10);
 
 app.listen(app.get('port'), function () {
   console.log("CPTEC Node Server started on port", app.get('port'));
