7b2bcd3695e2ba3e31810a38764a7ae0966b91d3
borellaster <borellaster@gmail.com>
2017-02-24 15:55:30 -0300

    alteracoes na tabela usuario, incluido mais dois campos

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,9 @@ module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
     name: DataTypes.STRING,
     username: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    password_tmp: DataTypes.STRING,
+    status: DataTypes.BOOLEAN
   }, {
     underscored: true,
     classMethods: {
