51ab44ff7d7fffb7af0443b4adc7ca30a4083633
Felipe Borella <borella@cittainformatica.com.br>
2017-03-27 15:50:52 -0300

    iniciado com a autenticacao

--- index.js
+++ index.js
@@ -31,65 +31,71 @@ app.use(express.static('public'));
 
 app.use(flash());
 setupPassport(app);
-//app.use(passport.initialize());
-//app.use(passport.session());
+
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()){
+    return next();
+  }
+  req.flash('error', 'You have to be logged in to access the page.')
+  res.redirect('/')
+}
 
 /*countries api*/
-app.get('/api/v1/countries/:page/:size', countries.findAll);
-app.get('/api/v1/countries/search/:page/:size/:name', countries.search);
-app.get('/api/v1/countries/:id', countries.findById);
-app.post('/api/v1/countries', countries.save);
-app.put('/api/v1/countries/:id', countries.update);
-app.delete('/api/v1/countries/:id', countries.delete);
+app.get('/api/v1/countries/:page/:size', isAuthenticated, countries.findAll);
+app.get('/api/v1/countries/search/:page/:size/:name', isAuthenticated, countries.search);
+app.get('/api/v1/countries/:id', isAuthenticated, countries.findById);
+app.post('/api/v1/countries', isAuthenticated, countries.save);
+app.put('/api/v1/countries/:id', isAuthenticated, countries.update);
+app.delete('/api/v1/countries/:id', isAuthenticated, countries.delete);
 
 /*states api*/
-app.get('/api/v1/states/:page/:size', states.findAll);
-app.get('/api/v1/states/search/:page/:size/:name', states.search);
-app.get('/api/v1/states/:id', states.findById);
-app.post('/api/v1/states', states.save);
-app.put('/api/v1/states/:id', states.update);
-app.delete('/api/v1/states/:id', states.delete);
+app.get('/api/v1/states/:page/:size', isAuthenticated, states.findAll);
+app.get('/api/v1/states/search/:page/:size/:name', isAuthenticated, states.search);
+app.get('/api/v1/states/:id', isAuthenticated, states.findById);
+app.post('/api/v1/states', isAuthenticated, states.save);
+app.put('/api/v1/states/:id', isAuthenticated, states.update);
+app.delete('/api/v1/states/:id', isAuthenticated, states.delete);
 
 /*cities api*/
-app.get('/api/v1/cities/:page/:size', cities.findAll);
-app.get('/api/v1/cities/search/:page/:size/:name', cities.search);
-app.get('/api/v1/cities/:id', cities.findById);
-app.post('/api/v1/cities', cities.save);
-app.put('/api/v1/cities/:id', cities.update);
-app.delete('/api/v1/cities/:id', cities.delete);
+app.get('/api/v1/cities/:page/:size', isAuthenticated, cities.findAll);
+app.get('/api/v1/cities/search/:page/:size/:name', isAuthenticated, cities.search);
+app.get('/api/v1/cities/:id', isAuthenticated, cities.findById);
+app.post('/api/v1/cities', isAuthenticated, cities.save);
+app.put('/api/v1/cities/:id', isAuthenticated, cities.update);
+app.delete('/api/v1/cities/:id', isAuthenticated, cities.delete);
 //app.get('/api/v1/cities/borella/teste/json2csv', cities.testeJson2Csv);
 
 /*variables api*/
-app.get('/api/v1/variables/:page/:size', variables.findAll);
-app.get('/api/v1/variables/search/:page/:size/:name', variables.search);
-app.get('/api/v1/variables/:id', variables.findById);
-app.post('/api/v1/variables', variables.save);
-app.put('/api/v1/variables/:id', variables.update);
-app.delete('/api/v1/variables/:id', variables.delete);
+app.get('/api/v1/variables/:page/:size', isAuthenticated, variables.findAll);
+app.get('/api/v1/variables/search/:page/:size/:name', isAuthenticated, variables.search);
+app.get('/api/v1/variables/:id', isAuthenticated, variables.findById);
+app.post('/api/v1/variables', isAuthenticated, variables.save);
+app.put('/api/v1/variables/:id', isAuthenticated, variables.update);
+app.delete('/api/v1/variables/:id', isAuthenticated, variables.delete);
 
 /*users api*/
-app.get('/api/v1/users/:page/:size', users.findAll);
-app.get('/api/v1/users/search/:page/:size/:name', users.search);
-app.get('/api/v1/users/:id', users.findById);
-app.post('/api/v1/users', users.save);
-app.put('/api/v1/users/:id', users.update);
-app.delete('/api/v1/users/:id', users.delete);
+app.get('/api/v1/users/:page/:size', isAuthenticated, users.findAll);
+app.get('/api/v1/users/search/:page/:size/:name', isAuthenticated, users.search);
+app.get('/api/v1/users/:id', isAuthenticated, users.findById);
+app.post('/api/v1/users', isAuthenticated, users.save);
+app.put('/api/v1/users/:id', isAuthenticated, users.update);
+app.delete('/api/v1/users/:id', isAuthenticated, users.delete);
 
 /*types api*/
-app.get('/api/v1/types/:page/:size', types.findAll);
-app.get('/api/v1/types/search/:page/:size/:name', types.search);
-app.get('/api/v1/types/:id', types.findById);
-app.post('/api/v1/types', types.save);
-app.put('/api/v1/types/:id', types.update);
-app.delete('/api/v1/types/:id', types.delete);
+app.get('/api/v1/types/:page/:size', isAuthenticated, types.findAll);
+app.get('/api/v1/types/search/:page/:size/:name', isAuthenticated, types.search);
+app.get('/api/v1/types/:id', isAuthenticated, types.findById);
+app.post('/api/v1/types', isAuthenticated, types.save);
+app.put('/api/v1/types/:id', isAuthenticated, types.update);
+app.delete('/api/v1/types/:id', isAuthenticated, types.delete);
 
-/*types api*/
-app.get('/api/v1/requests/:page/:size', requests.findAll);
-app.get('/api/v1/requests/search/:page/:size/:name', requests.search);
-app.get('/api/v1/requests/:id', requests.findById);
-app.post('/api/v1/requests', requests.save);
-app.put('/api/v1/requests/:id', requests.update);
-app.delete('/api/v1/requests/:id', requests.delete);
+/*requests api*/
+app.get('/api/v1/requests/:page/:size', isAuthenticated, requests.findAll);
+app.get('/api/v1/requests/search/:page/:size/:name', isAuthenticated, requests.search);
+app.get('/api/v1/requests/:id', isAuthenticated, requests.findById);
+app.post('/api/v1/requests', requests.save); //essa fica aberta pq o cara vai poder gravar 
+app.put('/api/v1/requests/:id', isAuthenticated, requests.update);
+app.delete('/api/v1/requests/:id', isAuthenticated, requests.delete);
 
 /*Native Queries*/
 app.get('/api/v1/native/countries/wrapper', countries.combo);
