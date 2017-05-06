1de658df9e49efea2c3b9cdacfd6201d4aef1dc2
borellaster <borellaster@gmail.com>
2017-05-06 14:46:51 -0300

    ajustado path da geracao de arquivo

--- server/controllers/requestProcessing.js
+++ server/controllers/requestProcessing.js
@@ -2,19 +2,24 @@ var schedule = require('node-schedule');
 var json2csv = require('json2csv');
 var fs = require('fs');
 var db = require('../models/index');
+var path = require('path');
+//var os = require('os');
 
 
 var rule = new schedule.RecurrenceRule();
-rule.minute = 00;
+rule.minute = 50;
 var fields = ['id', 'name'];
 
 var requestTimer = schedule.scheduleJob(rule, function(){
+
+    var rootPath = path.resolve(__dirname);
+    rootPath = rootPath.substring(0, rootPath.length -24);
 
     var result = {data: []};
     db.sequelize.query("select id, name from \"countries\" order by name ", { 
    		type:db.Sequelize.QueryTypes.SELECT}).then(function(countries) {
         var csv = json2csv({ data: countries, fields: fields, del: ';'});
-		fs.writeFile('C:/temp/req_2.csv', csv, function(err) {
+		fs.writeFile(rootPath + 'req_2.csv', csv, function(err) {
 		  if (err) throw err;
 		  console.log('CSV file created');
 		});
