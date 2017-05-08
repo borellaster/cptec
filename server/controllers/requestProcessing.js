48977c2172a152363252bff01594ad011fb9452f
Felipe Borella <borella@Felipes-MacBook-Air.local>
2017-05-07 21:01:04 -0300

    tentativa de criar um json e csv

--- server/controllers/requestProcessing.js
+++ server/controllers/requestProcessing.js
@@ -5,48 +5,52 @@ var db = require('../models/index');
 var path = require('path');
 request = require('../models/').request;
 var dateFormat = require('dateformat');
-
+var functions = require(__dirname + '/../tools/functions');
+var jsonfile = require('jsonfile');
 
 var rule = new schedule.RecurrenceRule();
-rule.minute = 26;
-var fields = ['valor', 'data', 'hora', 'variavel'];
+rule.minute = 54;
+var fields = ['value', 'date', 'time', 'variable'];
+var fieldNames = ['Valor', 'Data', 'Hora', 'Variavel'];
+var result = {data: []}; 
 
 var requestTimer = schedule.scheduleJob(rule, function(){
 
     var rootPath = path.resolve(__dirname);
     rootPath = rootPath.substring(0, rootPath.length -24);
+  
 
-    request.findAll({order: 'id', where: {status: 0}, include: {all: true}}).then(function (requests) {                      
+    request.findAll({order: 'id', where: {status: 0}, include: {all: true}}).then(function (requests) {                         
       for (var i = 0; i < requests.length; i++) {
-        var req = requests[0];
-        var query = " select ST_VALUE(RAST, ST_SETSRID(ST_MAKEPOINT("+req.location.coordinates+"), 4236)) as value, "+
-        " cast(date as date), time, variable "+
+        var req = requests[i];
+        var adjusted = functions.findQuadrant(req.location.coordinates[0], req.location.coordinates[1]);
+        var latitude = adjusted.lat;
+        var longitude = adjusted.lng; 
+
+        var query = " select ST_VALUE(RAST, ST_SETSRID(ST_MAKEPOINT("+longitude+", "+latitude+"), 4236)) as value, "+
+        " to_char(date, 'YYYY-MM-DD') as date, time, variable "+
         " from RASTER_DATA "+
         " where date between '"+dateFormat(req.start_date, "yyyy-mm-dd h:MM:ss")+"' and '"+dateFormat(req.end_date, "yyyy-mm-dd h:MM:ss")+"' "+
         " and variable in ('"+ req.variables + "')" +
         " order by variable, date, time ";
         db.sequelize.query(query, {type:db.Sequelize.QueryTypes.SELECT}).then(function(rasters) {
-            var csv = json2csv({ data: rasters, fields: fields, del: ';'});
-            fs.writeFile(rootPath + 'req'+req.id+'.csv', csv, function(err) {
-              if (err) throw err;
-              console.log('req'+req.id+'.csv created');
-            });            
+            console.log(req.type.extension);
+            if(req.type.extension = '.csv'){
+                var csv = json2csv({ data: rasters, fields: fields, fieldNames: fieldNames, del: ';'});
+                fs.writeFile(rootPath + 'req'+req.id+'.csv', csv, function(err) {
+                  if (err) throw err;
+                  console.log('req'+req.id+'.csv created');
+                });
+            } else if(req.type.extension = '.json'){
+                var file = rootPath + 'req'+req.id+'.json';
+                jsonfile.writeFile(file, rasters, function (err) {
+                  console.log('req'+req.id+'.json created');
+                });
+            }            
         }).catch(function (error) {            
         });         
           
       }
     }).catch(function (error) {      
-    });    
-
-    /*var result = {data: []};
-    db.sequelize.query("select id, name from \"countries\" order by name ", { 
-   		type:db.Sequelize.QueryTypes.SELECT}).then(function(countries) {
-        var csv = json2csv({ data: countries, fields: fields, del: ';'});
-		fs.writeFile(rootPath + 'req_2.csv', csv, function(err) {
-		  if (err) throw err;
-		  console.log('CSV file created');
-		});
-    }).catch(function (error) {
-      
-    });*/
+    });
 });
